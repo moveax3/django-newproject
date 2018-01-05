38ce790266cd557586938577ed8ef8a17d469db3
moveax3 <moveax3@gmail.com>
2018-01-05 18:10:50 +0500

    change task model

--- Makefile
+++ Makefile
@@ -19,3 +19,21 @@ build:
 
 stop:
 	docker-compose stop
+
+backup:
+	docker-compose exec db pg_dump -U user -a -Fc -f /fixtures/organiser.backup database
+
+restore: recreate_db migrate_db restore_db
+
+recreate_db:
+	docker-compose stop django
+	docker-compose exec db dropdb -U user database
+	docker-compose exec db createdb -U user database
+	docker-compose start django
+
+migrate_db:
+	docker-compose exec django python3 manage.py makemigrations
+	docker-compose exec django python3 manage.py migrate
+
+restore_db:
+	docker-compose exec db pg_restore -U user -a -d database /fixtures/organiser.backup
--- django/src/tasks/models.py
+++ django/src/tasks/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from targets.models import Target
 from model_utils.models import TimeStampedModel
+from model_utils import Choices
+from model_utils.fields import MonitorField, StatusField
 
 class Task(TimeStampedModel):
+    STATUS = Choices('Новая', 'В работе', 'Готово')
+    status = StatusField()
+    status_working_at = MonitorField(monitor='status', when=['В работе'])
+    status_complete_at = MonitorField(monitor='status', when=['Готово'])
+
     parent = models.ForeignKey(
         to              = 'self',
         blank           = True,
         null            = True,
         verbose_name    = "Родительская задача",
-        on_delete       = models.CASCADE
+        on_delete       = models.CASCADE,
+        related_name    = '+',
     )
     target = models.ForeignKey(
         to              = Target,
@@ -27,6 +35,14 @@ class Task(TimeStampedModel):
         blank           = False,
         null            = False,
         verbose_name    = "Описание"
+    )
+    is_blocked  = models.ForeignKey(
+        to              = 'self',
+        blank           = True,
+        null            = True,
+        verbose_name    = "Блокируется",
+        on_delete       = models.CASCADE,
+        related_name    = '+',
     )
 
     class Meta():
